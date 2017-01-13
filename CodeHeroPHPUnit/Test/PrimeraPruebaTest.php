f555d2bf8c7d443d54c868201e14913bba404aba
albsosa <inf.albsosa@gmail.com>
2017-01-13 11:46:31 -0600

    Primer commit
    Primera prueba lista donde true debe de ser true

---
+++ CodeHeroPHPUnit/Test/PrimeraPruebaTest.php
@@ -0,0 +1,10 @@
+<?php 
+namespace CodeHeroPHPUnit\Test;
+
+//Podemos Observar varias de las convenciones que dijimos anteriormente 
+//El nombre del archivo termina en test
+//El nombre de nuestra clase termina en Test y es igual al del archivo 
+//Extendemos de la clase PHPUnit_Framework_TestCase
+//Nuestra funcion comienza con test
+
+
